675c815f06ab6f45497886b08af197ca248b2fe7
Stephen Williams <107999+onato@users.noreply.github.com>
2025-01-27 15:24:49 +1300

    feat: Add modal for adding new timetable items in the tab component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
-import { FlatList } from "react-native";
+import { FlatList, Button, View } from "react-native";
+import EditTimetableModal from "../../components/EditTimetableModal";
+import { useState } from "react";
 import TimetableItem from "../../components/TimetableItem";
 import React from "react";
 
 export default function Tab() {
-  const timeframes = [
+  const [timeframes, setTimeframes] = useState([
     { id: "1", protocol: "High", start: "08:00", end: "10:00", enabled: true },
     { id: "2", protocol: "Low", start: "10:00", end: "12:00", enabled: false },
     { id: "3", protocol: "Bat", start: "12:00", end: "14:00", enabled: true },
@@ -14,13 +16,45 @@ export default function Tab() {
       end: "16:00",
       enabled: false,
     },
-  ];
+  ]);
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [newItem, setNewItem] = useState({
+    id: "",
+    protocol: "",
+    start: "",
+    end: "",
+    enabled: false,
+  });
+
+  const handleAdd = () => {
+    setNewItem({ id: (timeframes.length + 1).toString(), protocol: "", start: "", end: "", enabled: false });
+    setModalVisible(true);
+  };
+
+  const handleSave = (updatedItem) => {
+    setTimeframes([...timeframes, updatedItem]);
+    setModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setModalVisible(false);
+  };
 
   return (
-    <FlatList
-      data={timeframes}
-      renderItem={({ item }) => <TimetableItem item={item} />}
-      keyExtractor={(item) => item.id}
-    />
+    <View>
+      <Button title="Add" onPress={handleAdd} />
+      <EditTimetableModal
+        visible={modalVisible}
+        item={newItem}
+        onSave={handleSave}
+        onCancel={handleCancel}
+      />
+      <FlatList
+        data={timeframes}
+        renderItem={({ item }) => <TimetableItem item={item} />}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
   );
 }
