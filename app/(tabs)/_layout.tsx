6c2c42cc5469a1696d0f584b01d7aa3a8c98d6cb
Stephen Williams <107999+onato@users.noreply.github.com>
2025-02-20 13:55:49 +1300

    chore: Format _layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,34 +7,34 @@ import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 export default function TabLayout() {
   return (
     <>
-    <StatusBar backgroundColor={colors.docGreen} barStyle="light-content" />
-    <Tabs screenOptions={{
-      tabBarActiveTintColor: colors.docBlue,
-      headerStyle: { backgroundColor: colors.docGreen },
-      headerTintColor: '#FFFFFF'
-    }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Timetable',
-          tabBarIcon: ({ focused, color, size }) => <Ionicons size={size} name={focused ? "calendar" : "calendar-outline"} color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: 'Settings',
-          tabBarIcon: ({ focused, color, size }) => <Ionicons size={size} name={focused ? "settings" : "settings-outline"} color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="program"
-        options={{
-          title: 'Program',
-          tabBarIcon: ({ focused, color, size }) => <MaterialCommunityIcons size={size} name={focused ? "contactless-payment-circle" : "contactless-payment-circle-outline"} color={color} />,
-        }}
-      />
-    </Tabs>
+      <StatusBar backgroundColor={colors.docYellow} barStyle="dark-content" />
+      <Tabs screenOptions={{
+        tabBarActiveTintColor: colors.docBlue,
+        headerStyle: { backgroundColor: colors.docGreen },
+        headerTintColor: '#FFFFFF'
+      }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Timetable',
+            tabBarIcon: ({ focused, color, size }) => <Ionicons size={size} name={focused ? "calendar" : "calendar-outline"} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: 'Settings',
+            tabBarIcon: ({ focused, color, size }) => <Ionicons size={size} name={focused ? "settings" : "settings-outline"} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="program"
+          options={{
+            title: 'Program',
+            tabBarIcon: ({ focused, color, size }) => <MaterialCommunityIcons size={size} name={focused ? "contactless-payment-circle" : "contactless-payment-circle-outline"} color={color} />,
+          }}
+        />
+      </Tabs>
     </>
   );
 }
