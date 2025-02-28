c26662cdc85e67549c0796a60127385d93fb7572
Stephen Williams <107999+onato@users.noreply.github.com>
2025-02-28 15:20:08 +1300

    feat: Replace date picker with custom time picker component in modal

--- components/EditTimetableModal.tsx
+++ components/EditTimetableModal.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import { Modal, View, Text, StyleSheet } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
-import DatePicker from 'react-native-date-picker'
 import styles from '../styles';
 import colors from '../colors';
 import { Protocol } from '@onato/doc-nfc-module';
-import BorderedButton from '../components/BorderedButton';
 import { Timeframe } from '../models/Timeframe';
+import TimePicker from '../components/TimePicker';
 import TextButton from '../components/TextButton';
 import LogoTitle from '../components/LogoTitle';
 
@@ -22,34 +21,11 @@ const EditTimetableModal: React.FC<EditTimetableModalProps> = ({ visible, item, 
   const [start, setStart] = useState(item ? `${item.start_hour}:${item.start_minute.toString().padStart(2, '0')}` : '12:00');
   const [end, setEnd] = useState(item ? `${item.end_hour}:${item.end_minute.toString().padStart(2, '0')}` : '13:00');
 
-  const [showStartPicker, setShowStartPicker] = useState(false);
-  const [showEndPicker, setShowEndPicker] = useState(false);
-
-  const handleStartChange = (selectedDate: Date | undefined) => {
-    if (selectedDate) {
-      setStart(selectedDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }));
-    }
-    setShowStartPicker(false)
-  };
-
-  const handleEndChange = (selectedDate: Date | undefined) => {
-    if (selectedDate) {
-      setEnd(selectedDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }));
-    }
-    setShowEndPicker(false)
-  };
-
   const handleSave = () => {
     const [startHour, startMinute] = start.split(':').map(Number);
     const [endHour, endMinute] = end.split(':').map(Number);
     onSave({ ...item, protocol, start_hour: startHour, start_minute: startMinute, end_hour: endHour, end_minute: endMinute });
   };
-
-  function time(hour: number, minute: number) {
-    const date = new Date();
-    date.setHours(hour, minute, 0, 0);
-    return date;
-  }
 
   return (
     <View>
@@ -67,48 +43,17 @@ const EditTimetableModal: React.FC<EditTimetableModalProps> = ({ visible, item, 
                 <Picker.Item key={protocolValue} label={protocolValue} value={protocolValue} />
               ))}
             </Picker>
-            <Text style={styles.label}>Start</Text>
-            <BorderedButton
-              title={start}
-              onPress={() => setShowStartPicker(true)}
+            <TimePicker
+              label="Start"
+              time={start}
+              onTimeChange={setStart}
               disabled={protocol.indexOf("Tier1") !== -1}
             />
-            <DatePicker
-              title="Start Time"
-              locale="de_DE"
-              is24hourSource="locale"
-              modal
-              open={showStartPicker}
-              date={time(item.start_hour, item.start_minute)}
-              mode="time"
-              buttonColor={colors.docBlue}
-              dividerColor={colors.docYellow}
-              minuteInterval={5}
-              onConfirm={handleStartChange}
-              onCancel={() => {
-                setShowStartPicker(false)
-              }}
-            />
-
-            <Text style={styles.label}>Stop</Text>
-            <BorderedButton
-              title={end}
-              onPress={() => setShowEndPicker(true)}
+            <TimePicker
+              label="Stop"
+              time={end}
+              onTimeChange={setEnd}
               disabled={protocol.indexOf("Tier1") !== -1}
-            />
-            <DatePicker
-              title="Stop Time"
-              locale="de_DE"
-              is24hourSource="locale"
-              modal
-              open={showEndPicker}
-              date={time(item.end_hour, item.end_minute)}
-              mode="time"
-              minuteInterval={5}
-              onConfirm={handleEndChange}
-              onCancel={() => {
-                setShowEndPicker(false)
-              }}
             />
             {protocol.indexOf("Tier1") !== -1 && (
               <Text style={{ padding: 10 }}>
---
+++ components/TimePicker.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import DatePicker from 'react-native-date-picker';
+import BorderedButton from './BorderedButton';
+import colors from '../colors';
+
+interface TimePickerProps {
+  label: string;
+  time: string;
+  onTimeChange: (time: string) => void;
+  disabled: boolean;
+}
+
+const TimePicker: React.FC<TimePickerProps> = ({ label, time, onTimeChange, disabled }) => {
+  const [showPicker, setShowPicker] = useState(false);
+
+  const handleTimeChange = (selectedDate: Date | undefined) => {
+    if (selectedDate) {
+      onTimeChange(selectedDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }));
+    }
+    setShowPicker(false);
+  };
+
+  function timeToDate(hour: number, minute: number) {
+    const date = new Date();
+    date.setHours(hour, minute, 0, 0);
+    return date;
+  }
+
+  return (
+    <View>
+      <Text style={{ padding: 10 }}>{label}</Text>
+      <BorderedButton
+        title={time}
+        onPress={() => setShowPicker(true)}
+        disabled={disabled}
+      />
+      <DatePicker
+        title={label}
+        locale="de_DE"
+        is24hourSource="locale"
+        modal
+        open={showPicker}
+        date={time ? timeToDate(...time.split(':').map(Number).slice(0, 2) as [number, number]) : new Date()}
+        mode="time"
+        buttonColor={colors.docBlue}
+        dividerColor={colors.docYellow}
+        minuteInterval={5}
+        onConfirm={handleTimeChange}
+        onCancel={() => setShowPicker(false)}
+      />
+    </View>
+  );
+};
+
+export default TimePicker;
