8fe41a88dcc2d5e1ba9e1a8c395122485e239781
Stephen Williams <107999+onato@users.noreply.github.com>
2025-03-03 16:13:33 +1300

    refactor: Remove unused function

--- utils/TimeframeStore.js
+++ utils/TimeframeStore.js
@@ -1,12 +1,3 @@
-export const defaultTimeframes = () => {
-  return [
-          { id: "1", protocol: "High", start_hour: 8, start_minute: 0, end_hour: 10, end_minute: 0, enabled: true },
-          { id: "2", protocol: "Low", start_hour: 10, start_minute: 0, end_hour: 12, end_minute: 0, enabled: true },
-          { id: "3", protocol: "Bat", start_hour: 12, start_minute: 0, end_hour: 14, end_minute: 0, enabled: true },
-          { id: "4", protocol: "Tier1 Day", start_hour: 14, start_minute: 0, end_hour: 16, end_minute: 0, enabled: true },
-        ];
-};
-
 export const defaultNewItem = () => {
   return {
     id: "",
